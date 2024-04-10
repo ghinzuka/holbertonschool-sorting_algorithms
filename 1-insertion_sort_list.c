aec85d5bd6c2df46ee295a487eb2a42a0bafb857
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-10 15:42:02 +0200

    coding 1 insertion using a while loop

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * @list: pointer of pointer to list of integer
+ * 
+*/
+void insertion_sort_list(listint_t **list)
+{
+
+}
