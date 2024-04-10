ac0ff86e1190db70ad5fb4cc9a4bbc88cd6fb2af
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-10 14:00:14 +0200

    creating first files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order
+ * @array: array of integer
+ * @size: size of array
+ * 
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int temp = 0;
+	int index = 0;
+	
+}
