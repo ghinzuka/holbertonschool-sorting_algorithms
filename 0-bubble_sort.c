9bba34451c935d23349c04b6090dc1bbe17f80ab
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-10 14:31:36 +0200

    using two loops and a temp var to code the function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,11 +3,28 @@
  * bubble_sort - function that sorts an array of integers in ascending order
  * @array: array of integer
  * @size: size of array
- * 
+ *
 */
 void bubble_sort(int *array, size_t size)
 {
 	int temp = 0;
-	int index = 0;
-	
+	size_t i = 0;
+	size_t j = 0;
+
+		while (array && i < size - 1)
+		{
+			j = 0;
+				while (j < size - i - 1)
+				{
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = temp;
+					print_array(array, size);
+				}
+				j++;
+				}
+		i++;
+		}
 }
