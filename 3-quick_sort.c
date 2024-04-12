e2dd7a10aec74aaaa150b35cdcdc524d6ae0ba33
Elmael33 <mael.salet33@gmail.com>
2024-04-12 02:27:19 +0200

    first step of writing a function that sorts an array of integers in ascending order using the Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+
+/**
+ * quick_sort_divide - divide the array into two parts to organize it around the pivot
+ * @array: pointer of the array be sorted
+ * @lower: the lower element of the array
+ * @bigger: the bigger element of the array
+ * @size: size of the array
+*/
+
+int quick_sort_divide (int *array, int lower, int bigger, size_t size)
+{
+	int i = lower; int j = lower;
+	int pivot = bigger;
+
+	for (j = lower; j < bigger; j++)
+	{
+		if (j < pivot)
+		{
+			array[i] = array[j];
+			array[j] = array[i];
+		}
+	}
+}
+
+/**
+ * quick_sort_pivot - select an element in the array for being the pivot of your function
+ * @array: pointer of the array be sorted
+ * @lower: the lower element of the array
+ * @bigger: the bigger element of the array
+ * @size: size of the array
+*/
+
+int quick_sort_pivot (int *array, int lower, int bigger, size_t size)
+{
+	int pivot;
+
+	if (lower > bigger)
+	{
+		return;
+	}
+	if (lower < 0)
+	{
+		return;
+	}
+	int pivot = quick_sort_divide(array, lower, bigger, size);
+    quick_sort_pivot(array, lower, pivot - 1, size);
+    quick_sort_pivot(array, pivot + 1, bigger, size);
+}
+/**
+ * quick_sort - sorts an array of integers in ascending order using Quick sort
+ * @array: pointer of the array be sorted
+ * @size: size of number of elements in the array
+*/
+
+void quick_sort(int *array, size_t size)
+{
+
+}
